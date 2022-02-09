ea4c7a48a8d2106ab43ec63edf27279912397332
Manuel <manuel@pop-os.localdomain>
2022-02-09 13:23:34 -0300

    Modify createHome function

--- src/home/home.js
+++ src/home/home.js
@@ -4,36 +4,33 @@ import HomeImg from '../images/coffee.jpg';
 
 export function createHome() {
   const content = document.querySelector('#content');
-  const items = [
-    {
-      name: 'Expresso',
-      description: 'Lorem ipsum dolor sit amet. Lorem, ipsum dolor.',
-      price: '$3',
-    },
-    {
-      name: 'Expresso',
-      description: 'Lorem ipsum dolor sit amet. Lorem, ipsum dolor.',
-      price: '$3',
-    },
-    {
-      name: 'Expresso',
-      description: 'Lorem ipsum dolor sit amet. Lorem, ipsum dolor.',
-      price: '$3',
-    },
-    {
-      name: 'Expresso',
-      description: 'Lorem ipsum dolor sit amet. Lorem, ipsum dolor.',
-      price: '$3',
-    },
-    {
-      name: 'Expresso',
-      description: 'Lorem ipsum dolor sit amet. Lorem, ipsum dolor.',
-      price: '$3',
-    },
-    {
-      name: 'Expresso',
-      description: 'Lorem ipsum dolor sit amet. Lorem, ipsum dolor.',
-      price: '$3',
-    },
-  ];
+  const information = document.createElement('div');
+  const title = document.createElement('h1');
+  const spanTitle = document.createElement('span');
+  const description = document.createElement('h3');
+  const makeOrder = document.createElement('button');
+  const img = document.createElement('img');
+
+  information.id = 'information';
+  title.id = 'title';
+  description.id = 'description';
+  makeOrder.id = 'make-order';
+  makeOrder.classList.add('btn');
+  img.id = 'home-img';
+  img.src = HomeImg;
+  img.alt = 'Image of two coffee cups.';
+
+  title.textContent = 'Hello ';
+  spanTitle.textContent = 'Coffee!';
+  description.textContent = `Start your day with us! Lorem ipsum dolor sit amet consectetur,
+  adipisicing elit. Cupiditate, officia. Lorem ipsum dolor sit amet.`;
+  makeOrder.textContent = 'Make Your Order';
+
+  title.appendChild(spanTitle);
+  information.appendChild(title);
+  information.appendChild(description);
+  information.appendChild(makeOrder);
+  content.appendChild(information);
+  content.appendChild(img);
+  return content;
 }
