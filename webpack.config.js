4c49c2218e7a5eac854005a500b3c148b9465481
Manuel <manuel@pop-os.localdomain>
2022-02-10 22:13:38 -0300

    Change mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  devtool: 'inline-source-map',
-  //! Modify to production
-  mode: 'development',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
